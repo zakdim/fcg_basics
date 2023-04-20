4e15da05807c438cebd1a0d2a0c388274fa1abce
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2023-04-20 19:45:46 -0400

    s02-28 Configuring widgets & understanding objects

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 47, 5, 120),
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    ),
+  );
+}
