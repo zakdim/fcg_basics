484034f34765042805707fbe2ced43476837aa61
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2023-04-20 22:05:27 -0400

    s02-38 Practice: create a custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_roll_dice/gradient_container.dart';
+import 'package:fcg_basics/gradient_container.dart';
 
 void main(List<String> args) {
   runApp(
