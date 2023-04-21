17885c47f75e35df7a1e004234097fc506650514
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2023-04-20 21:24:08 -0400

    s02-31 How to configure widgets & objects

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,8 @@ void main(List<String> args) {
                 Color.fromARGB(255, 26, 2, 80),
                 Color.fromARGB(255, 45, 7, 98),
               ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight
             ),
           ),
           child: const Center(
