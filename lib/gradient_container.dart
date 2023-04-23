c10ca4fe470c758cbe1827f6fa9db5d8db730adb
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2023-04-23 18:20:11 -0400

    s02-44 Displaying images & using multiple constructor functions

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -8,6 +8,10 @@ const endAlignment = Alignment.bottomRight;
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.color1, this.color2, {super.key});
 
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
+
   final Color color1;
   final Color color2;
 
@@ -21,8 +25,11 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyledText('Hello World!'),
+      child: Center(
+        child: Image.asset(
+          'assets/images/dice-2.png',
+          width: 200,
+        ),
       ),
     );
   }
